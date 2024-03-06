e1dc862e34921567c12b09c671c4cf9431748774
xundaoxd <xundaoxd@outlook.com>
2024-03-06 22:31:07 +0800

    integ echarts

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,24 @@
+import Echart from '@/components/Echart';
+
 export default function Page() {
+    const option = {
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                data: [150, 230, 224, 218, 135, 147, 260],
+                type: 'line'
+            }
+        ]
+    };
     return (
         <main>
-            hello
+            <Echart option={option} className='mx-auto w-96 h-72' />
         </main>
     );
 }
---
+++ src/components/Echart.js
@@ -0,0 +1,11 @@
+'use client'
+import { useRef, useEffect } from 'react';
+import * as echarts from 'echarts';
+
+export default function Echart({ option, ...props }) {
+    const ref = useRef()
+    useEffect(() => {
+        echarts.init(ref.current).setOption(option)
+    }, [option])
+    return <div ref={ref} {...props}></div>
+}
