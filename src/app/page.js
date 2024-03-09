eaa7ef99ae64ca86c0f8ee261d3c33a70f1678de
xundaoxd <xundaoxd@outlook.com>
2024-03-09 19:44:41 +0800

    main layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,11 @@
-import Echart from '@/components/Echart';
-
 export default function Page() {
-    const option = {
-        xAxis: {
-            type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-        },
-        yAxis: {
-            type: 'value'
-        },
-        series: [
-            {
-                data: [150, 230, 224, 218, 135, 147, 260],
-                type: 'line'
-            }
-        ]
-    };
     return (
-        <main>
-            <Echart option={option} className='mx-auto w-96 h-72' />
-        </main>
+        <div className='flex flex-row bg-gray-400'>
+            <div className='w-72 h-screen shrink-0 grow-0 bg-white pt-12 flex flex-col items-center' style={{ border: '1px red solid' }}>
+                <h1>Echart 可视化</h1>
+            </div>
+            <div className='shrink grow' style={{ border: '1px blue solid' }}>
+            </div>
+        </div>
     );
 }
